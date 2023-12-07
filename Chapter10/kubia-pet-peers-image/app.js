010d63df69f1be400caef09115e04225d1ce9437
k8mas <k8mas@ylxt.com>
2023-12-07 14:06:32 +0800

    ch10, try sts, using dns-SRV

--- Chapter10/kubia-pet-peers-image/app.js
+++ Chapter10/kubia-pet-peers-image/app.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const dns = require('dns');
 
 const dataFile = "/var/data/kubia.txt";
-const serviceName = "kubia.default.svc.cluster.local";
+const serviceName = "kubia-sts-pet-svc-headless.k8sia.svc.cluster.local";
+// headless的服务名: kubia-sts-pet-svc-headless 
+// 命名空间：k8sia 
 const port = 8080;
 
 
@@ -28,22 +30,22 @@ function httpGet(reqOptions, callback) {
 }
 
 var handler = function(request, response) {
-  if (request.method == 'POST') {
+  if (request.method == 'POST') {  // 如果请求为POST，那么就是写数据
     var file = fs.createWriteStream(dataFile);
     file.on('open', function (fd) {
       request.pipe(file);
       response.writeHead(200);
       response.end("Data stored on pod " + os.hostname() + "\n");
     });
-  } else {
+  } else {   // 如果请求不是POST
     response.writeHead(200);
-    if (request.url == '/data') {
+    if (request.url == '/data') {  // 请求路径是/data，则读取当前pod中的数据
       var data = fileExists(dataFile) ? fs.readFileSync(dataFile, 'utf8') : "No data posted yet";
       response.end(data);
-    } else {
+    } else {                         // 请求路径不是/data， 则读取整个“存储集群”中所有pod的数据
       response.write("You've hit " + os.hostname() + "\n");
       response.write("Data stored in the cluster:\n");
-      dns.resolveSrv(serviceName, function (err, addresses) {
+      dns.resolveSrv(serviceName, function (err, addresses) {   // 获取整个“存储集群”中所有pod
         if (err) {
           response.end("Could not look up DNS SRV records: " + err);
           return;
@@ -52,7 +54,7 @@ var handler = function(request, response) {
         if (addresses.length == 0) {
           response.end("No peers discovered.");
         } else {
-          addresses.forEach(function (item) {
+          addresses.forEach(function (item) {    // 迭代整个“存储集群”中每一个pod
             var requestOptions = {
               host: item.name,
               port: port,
